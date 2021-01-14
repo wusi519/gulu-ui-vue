5a3a131769312a0601181c2b63fa4b19bc6e269e
FredXu <327742525@qq.com>
2021-01-14 22:14:13 +0800

    添加三个按钮点击事件

--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="clicked">
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
     <g-icon class="icon" v-if="icon&&!loading" :name="icon"></g-icon>
     <g-icon class=" loading icon" v-if="loading" name="loading"></g-icon>
     <div class="content">
@@ -24,11 +24,6 @@
         }
       }
     },
-    methods:{
-      clicked(){
-        this.$emit('click')
-      }
-    }
   };
 </script>
 
