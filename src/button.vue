77ab6b1c460536f6be05a7e58f4a57e623880b13
FredXu <327742525@qq.com>
2021-01-14 22:41:06 +0800

    创建button group

--- src/button.vue
+++ src/button.vue
@@ -28,72 +28,20 @@
 </script>
 
 <style lang="scss" scoped>
-  @keyframes spin {
-    0% {
-      transform: rotate(0deg)
-    }
-    100% {
-      transform: rotate(360deg)
-    }
-
-  }
-
-  .g-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    font: inherit;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
+  @keyframes spin {0% {transform: rotate(0deg) }100% {transform: rotate(360deg) }}
+  .g-button {font-size: var(--font-size); height: var(--button-height);padding: 0 1em;font: inherit;border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);background: var(--button-bg); display: inline-flex;justify-content: center; align-items: center;
     vertical-align: middle;
-
-    &:hover {
-      border-color: var(--border-color-hover)
-    }
-
-    &:active {
-      background-color: var(--button-active-bg);
-    }
-
-    &:focus {
-      outline: none;
-    }
-
-
-    > .content {
-      order: 2
-    }
-
-    > .icon {
-      order: 1;
-    }
-
-    > .loading {
-      animation: spin 1s infinite linear;
-    }
-
+    &:hover {border-color: var(--border-color-hover)}
+    &:active {background-color: var(--button-active-bg);}
+    &:focus { outline: none;}
+    > .content {order: 2}
+    > .icon {order: 1;}
+    > .loading {animation: spin 1s infinite linear;}
     &.icon-right {
-      > .icon {
-        order: 2;
-        margin-right: 0;
-        margin-left: .1em;
-      }
-
-      > .content {
-        order: 1
-      }
-
-      > .loading {
-        animation: spin 1s infinite linear;
-      }
-    }
-
-
-  }
+      > .icon {order: 2;margin-right: 0;margin-left: .1em;}
+      > .content {order: 1}
+      > .loading {animation: spin 1s infinite linear;}}}
 
 
 </style>
